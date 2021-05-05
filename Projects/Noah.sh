6b3f884104232ab99ed68ee5869ffc037bb1f8ea
Jishnu N <jishnun789@gmail.com>
2021-05-06 00:02:02 +0530

    Adding comments to the Noah Script

--- Projects/Noah.sh
+++ Projects/Noah.sh
@@ -9,9 +9,19 @@ cat<<'EOF'
 
 EOF
 
+#You can any ASCII art in here, search for ASCII art generator in GOOGLE.
+#nohup allows you run a command without hangup signals.
+#& is used to run the app in background
+#I added a custom command Hi_Noah in bash_aliases, so that whenever I type that command This script will be executed.
+#I did this to automate the First things I do in a day. And to give me a headstart in starting a day ;).
 
 nohup firefox -new-tab https://www.youtube.com/watch?v=-MJi7T4lX80&t=11578s &
+
+#This is what I listen to While I code :)
+
 sleep 2;
 firefox -new-tab gmail.com;
 sleep 1;
 firefox -new-tab github.com;
+
+#Sleep is added to create delay to avoid errors.
