9bde1f98fbd8dcdb85fac5284772117b8e1c168f
Jishnu N <jishnun789@gmail.com>
2021-06-03 22:06:48 +0530

    Automatic Path Environment Variable Updater Added

---
+++ Projects/path_adder.sh
@@ -0,0 +1,11 @@
+echo "Enter the Path of the File to add to the environment variable";
+
+read new_path_variable;
+
+if [ -d "$new_path_variable" || -f "$new_path_variable" ];
+then
+	PATH="$PATH:$new_path_variable";
+	echo "Path successfully added";
+else
+	echo "Invalid path, It does not exist in your system";
+fi
